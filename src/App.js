61d36760285994cc35b7db26806c5bdac5e715b8
f <f.farokhifar987@gmail.com>
2022-06-03 21:08:49 +0430

    Add Contact

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import AddContact from "./components/AddContact/AddContact";
 import ContactList from "./components/ContactList/ContactList";
 const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, { ...contact, id: Date.now() }]);
+  };
   return (
     <main className="App">
       <h2>contact app</h2>
-     <AddContact/>
-     <ContactList/>
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList />
     </main>
   );
 };
--- src/components/AddContact/AddContact.js
+++ src/components/AddContact/AddContact.js
@@ -1,5 +1,39 @@
-const AddContact = () => {
-    return ( <div>Add Contact</div> );
-}
- 
+import { useState } from "react";
+import "./AddContact.css";
+const AddContact = ({ addContactHandler }) => {
+  const [contact, setContact] = useState({ name: "", email: "" });
+  const changeHandler = (e) => {
+    setContact({ ...contact, [e.target.name]: e.target.value });
+  };
+  const submitForm = (e) => {
+    e.preventDefault();
+    addContactHandler(contact);
+    setContact({name:"",email:""})
+  };
+  return (
+    <form onSubmit={submitForm}>
+      <div className="formControl">
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          value={contact.name}
+          onChange={changeHandler}
+        />
+      </div>
+      <div className="formControl">
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          value={contact.email}
+          onChange={changeHandler}
+        />
+      </div>
+
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
 export default AddContact;
